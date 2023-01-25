995bed9181e91b6a9344e6f590dae4cf8681383b
Semih404 <119291380+Semih404@users.noreply.github.com>
2023-01-25 11:07:55 +0100

    Update page_inscription.php

--- page_inscription.php
+++ page_inscription.php
@@ -1,27 +1,62 @@
 <?php
+ob_start();
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $mysqlConnection = new PDO(
     'mysql:host=localhost;dbname=clone_insta;charset=utf8',
     'root',
     ''
 );
-// Récupération des données du formulaire
-$pseudo = $_POST['pseudo'];
-$password = $_POST['password'];
 
-// Vérification du pseudo
-$stmt = $mysqlConnection->prepare("SELECT * FROM users WHERE pseudo = :pseudo");
-$stmt->bindParam(':username', $pseudo);
-$stmt->execute();
+if(isset($_POST['pseudo']) && isset($_POST['password'])){
+    $pseudo = $_POST['pseudo'];
+    $password = $_POST['password'];
 
-// Vérification si pseudo existe déjà
-if($stmt->rowCount() > 0){
-    // Pseudo existe déjà, affichage d'un message d'erreur
-    echo "Pseudo déjà utilisé, veuillez en choisir un autre.";
+    // Vérification de l'existence du pseudo
+    $query = $mysqlConnection->prepare("SELECT * FROM users WHERE pseudo = :pseudo");
+    $query->bindParam(':pseudo', $pseudo);
+    $query->execute();
+    print_r($query->errorInfo());
+    
+    $user = $query->fetch();
+
+    // Vérification si le pseudo existe déjà
+    if($query->rowCount() > 0){
+        // Pseudo existe déjà, affichage d'un message d'erreur
+        echo "Pseudo déjà utilisé, veuillez en choisir un autre.";
+    } else {
+        // Pseudo n'existe pas, ajout des données à la base de données
+        $query = $mysqlConnection->prepare("INSERT INTO users (id, pseudo, password) VALUES (null, :pseudo, :password)");
+        $query->bindParam(':pseudo', $pseudo, PDO::PARAM_STR);
+        $query->bindParam(':password', $password, PDO::PARAM_STR);
+        if ($query->execute()) {
+          echo "Inscription réussie!";
+          header('Location: fyp.php');
+          exit();
+        }
+    }
 } else {
-    // Pseudo n'existe pas, ajout des données à la base de données
-    $stmt = $mysqlConnection->prepare("INSERT INTO users (pseudo, password) VALUES (:pseudo, :password)");
-    $stmt->bindParam(':pseudo', $pseudo);
-    $stmt->bindParam(':password', $password);
-    $stmt->execute();
-    echo "Inscription réussie!";
+    // Affiche un message d'erreur si les variables $_POST sont manquantes
+    echo 'Les variables $_POST sont manquantes';
 }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice</title>
+</head>
+<body>
+    <form action="page_inscription.php" method="post">
+        Pseudo : <input type="text" name="pseudo"><br>
+        Password : <input type="password" name="password"><br>
+        <input type="submit" value="soumettre">
+    </form>
+</body>
+</html>
